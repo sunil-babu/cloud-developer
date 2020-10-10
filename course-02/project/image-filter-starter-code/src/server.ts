e130ddae46d6f5398137cb759596b13bd2b36c65
Sunil Babu <sunilbabu.tvm@gmail.com>
2020-10-10 11:28:37 +0200

    Update server.ts
    Add typescript

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -28,7 +28,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-    app.get("/filteredimage", async (req, res) => {
+    app.get("/filteredimage", async (req:express.Request, res:express.Response) => { => {
      const imageUrl = req.query.image_url;
 
      // check imageUrl is valid
